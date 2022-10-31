e5a024f3a795c1f227106d2d177e42f9bcb8f6d9
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-10-31 14:39:06 +0100

    CHAP-01-INTRODUCTION

---
+++ CHAP-01-INTRODUCTION/src/main/java/dev/jzadi/Bonjour.kt
@@ -0,0 +1,23 @@
+package dev.jzadi
+
+/**
+ * Fichier Bonjour, créée le 31/10/2022 à 14:25
+ *
+ * @author Joachim Zadi
+ * @version 1.0 du 31/10/2022
+ */
+
+fun main() {
+    print("Bonjour ")
+    println("Joachim")
+    println("Comment trouvez-vous le Kotlin ?")
+
+    // Commentaire sur une ligne
+
+    /*
+    Commentaire
+    sur plusieurs
+    lignes
+    */
+}
+
